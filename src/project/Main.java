db00ed9f9209405d1090db2a7b0a05ab0ba8a264
Christos Kormaris <chriskormaris@gmail.com>
2017-08-16 22:34:55 +0300

    NLJ-SMJ

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,16 +1,5 @@
-/**
- * ATHENS UNIVERSITY OF ECONOMICS & BUSINESS 
- * MSc in Computer Science
- * Course: Theory & Fundamentals of Database Systems
- * 
- * Students: Chris Kormaris, Konstantinos Korovesis
- * 
- * Deadline: January 19, 2017
- */
-
 package project;
 
-/** Memory restriction implementation, using batch files. **/
 public class Main {
 	
 	public static void main(String[] args) {
