81c3f7fcd59ff60acfcc9fc9e0b3916df73b70f2
Christos Kormaris <chriskormaris@gmail.com>
2020-03-10 12:27:04 +0200

    Update compileAll.bat
    Minor changes. Replace "mkdir bin" with "if not exist bin mkdir bin". Remove unneeded "\", "." and ";" characters.

--- compileAll.bat
+++ compileAll.bat
@@ -1,3 +1,3 @@
-mkdir ./bin
-javac ./src/alternative_merge_threads/*.java ./src/create_random_relation/*.java ./src/merge_threads/*.java ./src/multiple_merge_threads/*.java ./src/project/*.java -d ./bin
-pause;
+if not exist bin mkdir bin
+javac src/alternative_merge_threads/*.java src/create_random_relation/*.java src/merge_threads/*.java src/multiple_merge_threads/*.java src/project/*.java -d bin
+pause
